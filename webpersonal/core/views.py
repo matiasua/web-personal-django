fbe9d8b9fd3845002927c69d5b604f9152d0e071
matiasua <matiasncp7@gmail.com>
2022-07-06 10:59:29 -0400

    joining front with back

---
+++ webpersonal/core/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+html_base = """
+    <h1>Mi Web Personal</h1>
+    <ul>
+        <li><a href="/">Portada</a></li>
+        <li><a href="/about/">Acerca de</a></li>
+        <li><a href="/contact/">Contacto</a></li>
+        <li><a href="/portfolio/">Portafolio</a></li>
+    </ul>
+"""
+
+
+def home(request):
+  return  render(request, "core/home.html")
+
+
+def about(request):
+    return render(request, "core/about.html")
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def portfolio(request):
+    return render(request, 'core/portfolio.html')
